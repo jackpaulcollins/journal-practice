b522fcc92586bf1f93297604bc959d62247933b2
Dino Nguyen and Jack Collins <jack@>
2019-03-11 16:45:00 -0700

    still workin on it

--- src/journal.js
+++ src/journal.js
@@ -8,3 +8,43 @@ Entry.prototype.countWords = function(){
   var splitCountedWord = this.body.split(" ").length;
   return splitCountedWord;
 }
+
+Entry.prototype.checkLetters = function() {
+  var splitString = this.body.split("");
+  var wordObject = {
+    vowels: [],
+    consonants: [],
+  }
+
+  for (var x=0; x < splitString.length; x++) {
+    if(/[aeiou]/i.test(splitString[x])) {
+      wordObject.vowels.push(splitString[x]);
+    } else if ((splitString[x] !== " ") && !(/[!?.,'"*&%1234567890#@%^*()/|+=-_]/.test(splitString[x])))
+       {
+        wordObject.consonants.push(splitString[x]);
+      }
+    }
+
+
+  return wordObject;
+}
+
+Entry.prototype.firstSentence = function(){
+  var splitSentence = this.body.split(" ");
+  var firstEight = splitSentence.slice(0, 8);
+  var joinFirstEight = firstEight.join(" ");
+  var splitFirstEight = joinFirstEight.split("");
+  for (var x=0; x < splitFirstEight.length; x++) {
+    if ((splitFirstEight[x] === ".") || (splitFirstEight[x] === "?") || (splitFirstEight[x] === "!")) {
+      var finalProduct = splitFirstEight.slice(0,x+1).join("");
+      console.log(finalProduct)
+      break;
+    }
+  }
+  return finalProduct;
+}
+
+
+// if ((splitFirstEight[x] != ".") || (splitFirstEight[x] != "?") || (splitFirstEight[x] != "!")) {
+//   return joinFirstEight;
+// }
--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ $(document).ready(function() {
     var entryInput = $(".entry").val();
     var newEntry = new Entry(entryTitle, entryInput);
     var wordCount = newEntry.countWords();
-    $(".displayEntries").append("<h2>" + newEntry.title + "</h2>" + "<br>" + "<h5>" + newEntry.body + "<h5>" + "<br>" + "<p>" + "Your entry is " + wordCount + " words long." + "</p>");
+    var checkLetters = newEntry.checkLetters();
+    var firstSent = newEntry.firstSentence();
+    $(".displayEntries").append("<h2>" + newEntry.title + "</h2>" + "<br>" + "<h5>" + newEntry.body + "<h5>" + "<br>" + "<p>" + "Your entry is " + wordCount + " words long." + "</p>" + "<br>" + "<p>" + "Amount of vowels in your entry: " + checkLetters.vowels.length + "</p>" + "<br>" + "Amount of consonants in your entry: " + "<p>" + checkLetters.consonants.length + "</p>" + firstSent);
+
   });
 });
